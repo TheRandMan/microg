93166896a2fa8c3c59064de327d1f44dd1ebd116
TheRandMan <33759959+TheRandMan@users.noreply.github.com>
2019-07-09 20:06:39 -0700

    Update Android.mk

--- libjni_latinimegoogle/Android.mk
+++ libjni_latinimegoogle/Android.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libjni_latinimegoogle
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := libjni_latinimegoogle.so
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_SRC_FILES := libjni_latinimegoogle.so
+LOCAL_PACKAGE_NAME := libjni_latinimegoogle.so
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := system/lib64
+#LOCAL_MODULE_PATH := system/lib64
 include $(BUILD_PREBUILT)
